c96b9e57a6c008ceb82e95fb3f428386d9af5e8a
ArtemLevin <python_study_2022@mail.ru>
2023-10-07 00:29:38 +0300

    ht 1 task 1 final cut

--- ht_1_task_1.py
+++ ht_1_task_1.py
@@ -1,1 +1,20 @@
-a, b, c = [int (x) for x in input("Enter the data separated by a space -----> ").split()]
+a, b, c = [int(x) for x in input("Enter the data separated by a space -----> ").split()]
+sideList = [a, b, c]
+
+
+def triangle(sideList):
+    for i in range(len(sideList)):
+        sideSum = 0
+        for j in range(len(sideList)):
+            if i != j:
+                sideSum += sideList[j]
+        if sideList[i] >= sideSum: return print("Треугольник не существует")
+    if len(set(sideList)) == 1:
+        return print("Треугольник существует" + "\n" + "Треугольник равносторонний")
+    elif len(set(sideList)) == 2:
+        return print("Треугольник существует" + "\n" + "Треугольник равнобедренный")
+    elif len(set(sideList)) == 3:
+        return print("Треугольник существует" + "\n" + "Треугольник разносторонний")
+
+
+triangle(sideList)
