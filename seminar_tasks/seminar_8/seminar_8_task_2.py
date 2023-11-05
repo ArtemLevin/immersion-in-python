4a2fafbe6e2e4156995deb445ce59f0c5808c1a5
ArtemLevin <python_study_2022@mail.ru>
2023-11-05 20:28:24 +0300

    sem 8 task 2

---
+++ seminar_tasks/seminar_8/seminar_8_task_2.py
@@ -0,0 +1,23 @@
+import json
+import csv
+import os
+
+from seminar_tasks.seminar_8.seminar_8_task_1 import user_id_by_access_level
+
+FILENAME = "users.json"
+
+user_id_by_access_level()
+
+with (
+    open(FILENAME,"r") as jsonFile,
+    open("users.csv", "w") as csvFile ):
+
+    jsonFileContent = json.load(jsonFile)
+    print(jsonFileContent)
+    all_data =[["access value", "id", "name"]]
+    for x in jsonFileContent.keys():
+        for el in jsonFileContent[x]:
+            all_data.append([x, list(el.items())[0][0], list(el.items())[0][1]])
+    print(all_data)
+    csv_writer=csv.writer(csvFile, dialect="excel", delimiter=" ")
+    csv_writer.writerows(all_data)
